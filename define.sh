fe82235efcde081206f83b8be51179950f23ad6b
JayArch97 <jayquintanilla2@gmail.com>
2026-08-10 15:41:39 -0600

    adding examples

--- define.sh
+++ define.sh
@@ -3,4 +3,8 @@ word=$1
 
 definition=$(curl "https://freedictionaryapi.com/api/v1/entries/de/$word?translations=true&pretty=true" | jq '.entries[].senses[].definition')
 
+examples=$(curl "https://freedictionaryapi.com/api/v1/entries/de/$word?translations=true&pretty=true" | jq '.entries[].senses[].examples')
+
 notify-send -h string:bgcolor:#FFFFFF -h string:fgcolor:#000000 -h string:frcolor:#FFFFFF "$definition"
+
+notify-send -h string:bgcolor:#FFFFFF -h string:fgcolor:#000000 -h string:frcolor:#FFFFFF "$examples"
